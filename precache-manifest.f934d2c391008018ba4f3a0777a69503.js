e37c39fa78365d7e67a6f1fe76df4b44f0842ec2
Tito Okeke <innowhat@gmail.com>
2019-08-05 23:25:53 +0300

    Updates

---
+++ precache-manifest.f934d2c391008018ba4f3a0777a69503.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da284fd81f4b88c27794a019899cd50d",
+    "url": "/vnicorn-jokes-quotes-generator/index.html"
+  },
+  {
+    "revision": "2065d4ec7b3e9ac01c84",
+    "url": "/vnicorn-jokes-quotes-generator/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "45f2bc3f5a6c5b41a82c",
+    "url": "/vnicorn-jokes-quotes-generator/static/js/2.50fd81c0.chunk.js"
+  },
+  {
+    "revision": "2065d4ec7b3e9ac01c84",
+    "url": "/vnicorn-jokes-quotes-generator/static/js/main.2fdbb580.chunk.js"
+  },
+  {
+    "revision": "fa83c20cae4ef41ca1f1",
+    "url": "/vnicorn-jokes-quotes-generator/static/js/runtime~main.c056305e.js"
+  },
+  {
+    "revision": "14106da2c90faa03d6d446c49c5fd1d5",
+    "url": "/vnicorn-jokes-quotes-generator/static/media/vnicorn.14106da2.png"
+  }
+]);
